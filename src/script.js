ac3afef7230bdf7b23cd42d27f696120270ef62d
Hiroki <hiroki11hanada@gmail.com>
2021-07-21 11:26:38 +0900

    objects and 3d models

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 import * as dat from 'dat.gui'
 
 // Debug
@@ -12,17 +13,72 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+//group
+const modelGroup = new THREE.Group();
+
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+
+const planeGeometry = new THREE.PlaneGeometry( 100, 50 );
+let loader = new THREE.TextureLoader();
+loader.load(
+    "./textures/world-map-world-map-in-grey-hd-png.png",
+    (texture) => {
+        let planeMaterial = new THREE.MeshBasicMaterial( {map: texture} );
+        let plane = new THREE.Mesh( planeGeometry, planeMaterial );
+        scene.add( plane );
+    },
+    undefined,
+    (err) => {console.error(err)}
+);
+
+const geometry = new THREE.SphereGeometry( 1, 32, 32 );
+const cylinderGeometry = new THREE.CylinderGeometry( 1.2, 0.2, 3, 32);
+
+//loading 3D models
+const fbxLoader = new FBXLoader()
+fbxLoader.load(
+    'models/xbot.fbx',
+    (object) => {
+        object.scale.set(.05, .05, .05)
+        object.rotateX(Math.PI / 2)
+        modelGroup.add(object)
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
+    }
+)
+
+
 
 // Materials
 
 const material = new THREE.MeshBasicMaterial()
 material.color = new THREE.Color(0xff0000)
 
+
+
 // Mesh
 const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+const cylinder = new THREE.Mesh(cylinderGeometry,material)
+cylinder.position.z = 2;
+cylinder.rotateX(- Math.PI / 2)
+sphere.position.z = 4;
+modelGroup.add(cylinder)
+modelGroup.add(sphere)
+
+gui.add(modelGroup.position, 'x').min(-50).max(50).step(1);
+gui.add(modelGroup.position, 'y').min(-25).max(25).step(1);
+
+scene.add(modelGroup);
+
+
+//grid
+const gridHelper = new THREE.GridHelper( 100, 10 );
+
+scene.add( gridHelper );
 
 // Lights
 
@@ -59,10 +115,10 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
+const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 0.1, 1000 );
+camera.position.z = 40;
+camera.position.y = -20;
+camera.lookAt(0,0,0);
 scene.add(camera)
 
 // Controls
@@ -77,6 +133,11 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+//controls
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.target.set(0, 1, 0)
 
 /**
  * Animate
