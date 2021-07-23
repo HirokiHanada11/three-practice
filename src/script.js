a16fe086e39689c5e81adb844270622cc89621c3
Hiroki <hiroki11hanada@gmail.com>
2021-07-23 15:35:42 +0900

    created 3d world map

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 import * as dat from 'dat.gui'
+import { MeshBasicMaterial } from 'three'
 
 // Debug
 const gui = new dat.GUI()
@@ -14,65 +15,87 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 //group
-const modelGroup = new THREE.Group();
+const planeGroup = new THREE.Group();
 
 // Objects
-
-const planeGeometry = new THREE.PlaneGeometry( 100, 50 );
 let loader = new THREE.TextureLoader();
-loader.load(
-    "./textures/world-map-world-map-in-grey-hd-png.png",
-    (texture) => {
-        let planeMaterial = new THREE.MeshBasicMaterial( {map: texture} );
-        let plane = new THREE.Mesh( planeGeometry, planeMaterial );
-        scene.add( plane );
-    },
-    undefined,
-    (err) => {console.error(err)}
-);
+//let texture = loader.load("./textures/world-map-colored.jpg");
+//let height = loader.load('./textures/world-height-map.png');
 
-const geometry = new THREE.SphereGeometry( 1, 32, 32 );
-const cylinderGeometry = new THREE.CylinderGeometry( 1.2, 0.2, 3, 32);
+let height = loader.load('./textures/monochrome-height.jpg');
+let texture = loader.load('./textures/monochrome-height.jpg');
 
-//loading 3D models
-const fbxLoader = new FBXLoader()
-fbxLoader.load(
-    'models/xbot.fbx',
-    (object) => {
-        object.scale.set(.05, .05, .05)
-        object.rotateX(Math.PI / 2)
-        modelGroup.add(object)
-    },
-    (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-    },
-    (error) => {
-        console.log(error)
-    }
-)
+const landGeometry = new THREE.PlaneBufferGeometry( 100, 50, 1012, 1012 );
+
+let landMaterial = new THREE.MeshStandardMaterial( {
+    map: texture,
+    displacementMap: height,
+    displacementScale: 1.5,
+} );
+let landPlane = new THREE.Mesh( landGeometry, landMaterial );
+planeGroup.add( landPlane );
+
+const waterGeometry = new THREE.PlaneBufferGeometry( 100, 50, 16, 16);
+
+let waterMap = loader.load('./textures/water-normal-map.jpg');
+waterMap.wrapS = THREE.RepeatWrapping;
+waterMap.wrapT = THREE.RepeatWrapping;
+waterMap.repeat.set(4,2);
+
+let waterMaterial = new THREE.MeshStandardMaterial({
+    color: new THREE.Color('skyblue'),
+    normalMap: waterMap
+})
+
+// let waterMaterial = new MeshBasicMaterial({color: new THREE.Color('skyblue'),});
+let waterPlane = new THREE.Mesh( waterGeometry, waterMaterial );
+planeGroup.add(waterPlane);
+waterPlane.position.z=0.5
+scene.add(planeGroup);
+
+
+// const geometry = new THREE.SphereGeometry( 1, 32, 32 );
+// const cylinderGeometry = new THREE.CylinderGeometry( 1.2, 0.2, 3, 32);
+
+// //loading 3D models
+// const fbxLoader = new FBXLoader()
+// fbxLoader.load(
+//     'models/xbot.fbx',
+//     (object) => {
+//         object.scale.set(.05, .05, .05)
+//         object.rotateX(Math.PI / 2)
+//         modelGroup.add(object)
+//     },
+//     (xhr) => {
+//         console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+//     },
+//     (error) => {
+//         console.log(error)
+//     }
+// )
 
 
 
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+// const material = new THREE.MeshBasicMaterial()
+// material.color = new THREE.Color(0xff0000)
 
 
 
 // Mesh
-const sphere = new THREE.Mesh(geometry,material)
-const cylinder = new THREE.Mesh(cylinderGeometry,material)
-cylinder.position.z = 2;
-cylinder.rotateX(- Math.PI / 2)
-sphere.position.z = 4;
-modelGroup.add(cylinder)
-modelGroup.add(sphere)
+// const sphere = new THREE.Mesh(geometry,material)
+// const cylinder = new THREE.Mesh(cylinderGeometry,material)
+// cylinder.position.z = 2;
+// cylinder.rotateX(- Math.PI / 2)
+// sphere.position.z = 4;
+// modelGroup.add(cylinder)
+// modelGroup.add(sphere)
 
-gui.add(modelGroup.position, 'x').min(-50).max(50).step(1);
-gui.add(modelGroup.position, 'y').min(-25).max(25).step(1);
+// gui.add(modelGroup.position, 'x').min(-50).max(50).step(1);
+// gui.add(modelGroup.position, 'y').min(-25).max(25).step(1);
 
-scene.add(modelGroup);
+// scene.add(modelGroup);
 
 
 //grid
@@ -80,13 +103,20 @@ const gridHelper = new THREE.GridHelper( 100, 10 );
 
 scene.add( gridHelper );
 
+gridHelper.rotateX(Math.PI / 2);
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.x = 30
+pointLight.position.y = 0
+pointLight.position.z = 40
 scene.add(pointLight)
+
+const pointLight2 = new THREE.PointLight(0xffffff, 0.5)
+pointLight2.position.x = -30
+pointLight2.position.y = 0
+pointLight2.position.z = 40
+scene.add(pointLight2)
 
 /**
  * Sizes
@@ -121,9 +151,9 @@ camera.position.y = -20;
 camera.lookAt(0,0,0);
 scene.add(camera)
 
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
+//Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 /**
  * Renderer
@@ -134,10 +164,10 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-//controls
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.enableDamping = true
-controls.target.set(0, 1, 0)
+// //controls
+// const controls = new OrbitControls(camera, renderer.domElement)
+// controls.enableDamping = true
+// controls.target.set(0, 1, 0)
 
 /**
  * Animate
@@ -151,10 +181,17 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+    //sphere.rotation.y = .5 * elapsedTime
 
     // Update Orbital Controls
     // controls.update()
+
+    // pointLight.position.x = 60 * Math.sin(elapsedTime);
+    // pointLight.position.z = 60 * Math.cos(elapsedTime);
+    // pointLight2.position.x = -60 * Math.sin(elapsedTime);
+    // pointLight2.position.z = -60 * Math.cos(elapsedTime);
+
+    waterPlane.material.normalScale.set( Math.sin(elapsedTime), Math.cos(elapsedTime));
 
     // Render
     renderer.render(scene, camera)
